eca38d5c227e7689fa42e4c8bbe425e329f07bbf
matrigato <51022834+matrigato@users.noreply.github.com>
2025-06-25 00:05:04 -0300

    Update index.sql

--- sql/index.sql
+++ sql/index.sql
@@ -1,13 +1,13 @@
 START TRANSACTION;
 
-CREATE INDEX projeto.IdxAlunoAtivo ON projeto.matricula (aluno)
+CREATE INDEX IdxAlunoAtivo ON projeto.matricula (aluno)
 	WHERE status = 'ativa';
 
-CREATE INDEX projeto.IdxAvaliacaoMedia ON projeto.matricula
-	((classificacao_didatica + classificacao_material + classificacao_relevancia + classificacao_infraestrutura) / 4);
+--CREATE INDEX IdxAvaliacaoMedia ON projeto.matricula
+--	((classificacao_didatica + classificacao_material + classificacao_relevancia + classificacao_infraestrutura) / 4);
 
-CREATE INDEX projeto.IdxNomeUsuarioUpper ON projeto.matricula (Upper(usuario.nome))
+CREATE INDEX IdxNomeUsuarioUpper ON projeto.usuario (Upper(nome));
 
-CREATE INDEX projeto.IdxVinculoUsuarioUpper ON projeto.matricula (Upper(usuario.vinculo))
+CREATE INDEX IdxVinculoUsuarioUpper ON projeto.usuario (Upper(vinculo));
 	
 COMMIT;
